60cd3d8fb27db6388fcad8ee0d260a17180e1f6f
Leon Košak <leon.kosak@gmail.com>
2023-11-28 14:51:14 +0100

    frontpage changes

--- Docs/astro.config.mjs
+++ Docs/astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
 		starlight({
 			title: 'Docs for Nexavia Enterprise',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/kuem-si',
 			},
 			sidebar: [
 				{
