87c11097a89291a4a0e112a2eb2792e80cc598bb
Leon Košak <leon.kosak@gmail.com>
2024-03-01 08:25:48 +0100

    customcss file and configuration added.

--- Docs/astro.config.mjs
+++ Docs/astro.config.mjs
@@ -6,6 +6,10 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Docs for Nexavia Enterprise',
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+			],
 			social: {
 				github: 'https://github.com/kuem-si',
 			},
