1e3139f0b933cfe1d3a6b9a509dd6a13de33c703
Leon Košak <leon.kosak@gmail.com>
2025-01-31 11:22:59 +0100

    grid master-detail description ui & ux

--- Docs/astro.config.mjs
+++ Docs/astro.config.mjs
@@ -54,7 +54,8 @@ export default defineConfig({
 								{ label: 'Grid - Column Sorting', slug: 'user/general-ui-ux-patterns/grid-column-sorting' },
 								{ label: 'Grid - Column Customizations', slug: 'user/general-ui-ux-patterns/grid-column-customizations' },
 								{ label: 'Grid - Row Selection', slug: 'user/general-ui-ux-patterns/grid-row-selection' },
-								{ label: 'Grid - Pagination', slug: 'user/general-ui-ux-patterns/grid-pagination' }
+								{ label: 'Grid - Pagination', slug: 'user/general-ui-ux-patterns/grid-pagination' },
+								{ label: 'Grid - Master-Detail', slug: 'user/general-ui-ux-patterns/grid-master-detail' }
 							]
 						},
 						{
